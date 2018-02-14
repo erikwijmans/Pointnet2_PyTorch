b4cd2c7dabcb4d763c81548ae5a3887f38b5619a
erikwijmans <ewijmans2@gmail.com>
2018-02-14 12:30:36 -0500

    Hack fix

--- __init__.py
+++ __init__.py
@@ -1,1 +1,4 @@
-from .utils import pointnet2_modules, pointnet2_utils, pytorch_utils
+import os.path as osp
+import sys
+sys.path = [osp.join(osp.dirname(__file__), 'utils')] + sys.path
+import pointnet2_modules, pointnet2_utils, pytorch_utils
